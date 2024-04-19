8d766892b8ee8aa575edcf17112d8bc9e00d1fd0
rahul4151 <rahul.ghosh010010@gmail.com>
2024-04-19 17:54:53 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# app.py
+#hello
+import streamlit as st
+
+# Import your different pages
+from home import home_page
+from contact import contact_page
+
+# Define function to select page
+def main():
+    st.sidebar.title('Navigation')
+    
+    page = st.sidebar.selectbox('Go to', ['Home','Contact'])
+
+    
+
+    if page == 'Home':
+        home_page()
+    elif page == 'Contact':
+        contact_page()
+
+if __name__ == '__main__':
+    main()
